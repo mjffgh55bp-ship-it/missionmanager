ddd22a59a2b09a2252a5756ea8ca3beea8bb6603
עמית פלד <amitpeled2001@gmail.com>
2026-02-12 15:01:19 +0000

    File changes

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -770,8 +770,7 @@ export default function Schedule() {
                     </div>
                   </CardContent>
                 </Card>
-              );
-            })();
+              });
             })()}
           </div>
         )}
