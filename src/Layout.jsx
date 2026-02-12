ba7abf6888db202a428c55ee3157b840d9f28758
dor fintz <dorfintz@gmail.com>
2026-02-12 07:47:51 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -174,16 +174,16 @@ export default function Layout({ children }) {
       {/* Main Content */}
       <main className="min-h-screen flex flex-col w-full">
         <header className="bg-white border-b border-gray-200 px-6 py-4">
-          <div className="flex items-center gap-4 justify-end">
-            <h1 className="text-xl font-bold text-gray-900 flex-1 text-right" dir="rtl">מנהל משימות</h1>
+          <div className="flex items-center gap-4 justify-between">
             <button 
               onClick={() => setSidebarOpen(!sidebarOpen)}
-              className="hover:bg-gray-100 p-2 rounded-lg transition-colors duration-200"
+              className="hover:bg-gray-100 p-2 rounded-lg transition-colors duration-200 order-2"
             >
               <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
               </svg>
             </button>
+            <h1 className="text-xl font-bold text-gray-900 text-right order-1" dir="rtl">מנהל משימות</h1>
           </div>
         </header>
 
